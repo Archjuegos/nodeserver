09bf271046f499bd897c2024c129ee6298f0c4b9
Archjuegos <167877311+Archjuegos@users.noreply.github.com>
2024-04-25 19:10:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,18 @@ app.post('/validateToken', (req, res) => {
         res.json({ username: 'Richaldo', wallet: '1234' });
     } else if(tokenCrypto === 'efgh5678') {
         res.json({ username: 'Vrowinaldo', wallet: '5678' });
+    } 
+	else if(tokenCrypto === 'ujkl9123') {
+        res.json({ username: 'Testing1', wallet: '9123' });
+    } 
+	else if(tokenCrypto === 'mnop4567') {
+        res.json({ username: 'Testing2', wallet: '4567' });
+    } 
+	else if(tokenCrypto === 'qrst8912') {
+        res.json({ username: 'Testing3', wallet: '8912' });
+    } 
+	else if(tokenCrypto === 'uvwx3456') {
+        res.json({ username: 'Testing4', wallet: '3456' });
     } else {
         res.status(401).json({ message: 'Token inválido' }); // Devolver un error 401 si el token es inválido
     }
@@ -30,6 +42,14 @@ app.post('/validateUser', (req, res) => {
     if (username === 'Richaldo' && password === '12345678') { // Comprobar si el tokenCrypto es igual a '1234'
         res.json({ user: 'okay'});
     } else if(username === 'Vrowinaldo' && password === '12345678') {
+        res.json({ user: 'okay'});
+    } else if(username === 'Testing1' && password === '123456') {
+        res.json({ user: 'okay'});
+    } else if(username === 'Testing2' && password === '123456') {
+        res.json({ user: 'okay'});
+    } else if(username === 'Testing3' && password === '123456') {
+        res.json({ user: 'okay'});
+    } else if(username === 'Testing4' && password === '123456') {
         res.json({ user: 'okay'});
     } else {
         res.status(401).json({ message: 'Usuario incorrecto' }); // Devolver un error 401 si el token es inválido
